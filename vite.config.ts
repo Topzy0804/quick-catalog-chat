4429886102b0fb939d681883083e32346b21f1b4
unknown <azeezullahtolulope1004@gmail.com>
2026-07-06 17:36:08 +0100

    vercel hosting preparation

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
@@ -18,10 +19,9 @@ export default defineConfig({
     tsConfigPaths(),
     tailwindcss(),
     tanstackStart({
-      // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR entry wrapper)
-      // nitro/vite builds from this
       server: { entry: "server" },
     }),
+    nitro(), // <-- add this
     viteReact(),
   ],
 });
